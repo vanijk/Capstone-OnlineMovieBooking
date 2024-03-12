1f0906ea563aafcd005b5dfc37b02b6deac12244
vanijk <vani.jk@gmail.com>
2024-03-12 17:02:44 -0400

    Spring Initilaizer - Basic Project

---
+++ src/main/java/com/capstone/onlinemoviebooking/CapstoneOnlineMovieBookingApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.onlinemoviebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneOnlineMovieBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneOnlineMovieBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/onlinemoviebooking/CapstoneOnlineMovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.onlinemoviebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneOnlineMovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
